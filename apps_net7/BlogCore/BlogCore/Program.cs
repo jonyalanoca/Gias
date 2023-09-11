608d4bec07983f2e960537ed35bd692fb136be98
jonyalanoca <jonyalanoca2@gmail.com>
2023-09-11 02:02:44 -0300

    blogcore

---
+++ apps_net7/BlogCore/BlogCore/Program.cs
@@ -0,0 +1,49 @@
+using BlogCore.AccesoDatos.Data.Repository;
+using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using BlogCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("ConexionSQL") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI();
+builder.Services.AddControllersWithViews();
+
+
+//Agregar contenedor de trabajo
+builder.Services.AddScoped<IContenedorTrabajo, ContenedorTrabajo>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Cliente}/{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
